3178ed220a445d748666fc3408b6cc20d1f51e05
Elliot Hatch <elliot.hatch@gmail.com>
2018-12-27 04:14:54 -0700

    Add some server configs

--- arch/core/dotfiles/.zshenv
+++ arch/core/dotfiles/.zshenv
@@ -11,6 +11,8 @@ export LANG=en_US.UTF-8
 
 export PATH=$PATH:~/.local/bin
 
+export GOPATH=~/go
+
 export FZF_DEFAULT_COMMAND='ag -l --hidden --path-to-ignore $HOME/.config/ag/.ignore'
 
 export I3BLOCKS_SCRIPT_DIR=$HOME/.config/i3blocks/scripts
--- arch/server/setup.sh
+++ arch/server/setup.sh
@@ -3,3 +3,16 @@
 sudo machinectl shell gdm@ /bin/bash
 export GSETTINGS_BACKEND=dconf
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+
+# set up dynamic dns
+go get github.com/anaganisk/digitalocean-dynamic-dns-ip
+mkdir ~/build
+cd ~/build
+git clone https://github.com/anaganisk/digitalocean-dynamic-dns-ip.git
+cd digitalocean-dynamic-dns-ip
+go build digitalocean-dynamic-dns-ip.go
+
+sudo cp ./etc/systemd/system/ddns.service /etc/systemd/system
+sudo cp ./etc/systemd/system/ddns.timer /etc/systemd/system
+sudo systemctl start ddns.timer
+sudo systemctl enable ddns.timer
