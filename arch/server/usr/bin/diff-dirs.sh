34385dc7acd5b2fd7f4a1ff5cc182496b4d9cf9c
Elliot Hatch <elliot.hatch@gmail.com>
2019-04-11 20:24:52 -0600

    Make diff-dirs much prettier with tree view, only post file changes in fresh4less share

--- arch/server/usr/bin/diff-dirs.sh
+++ arch/server/usr/bin/diff-dirs.sh
@@ -3,4 +3,33 @@ DIR1="$(readlink -f "$1")"
 DIR2="$(readlink -f "$2")"
 
 >&2 echo "Comparing $DIR1 to $DIR2"
-diff <(find $DIR1 | sed -r "s|^.*$DIR1/?||" | sort) <(find $DIR2 | sed -r "s|^.*$DIR2/?||" | sort) | sed 's/>/Added/' | sed 's/</Deleted/'
+diff -U 0 <(find $DIR1 -printf ':%y:%P\n') <(find $DIR2 -printf ':%y:%P\n') | grep -v -E '^(@@|---|\+\+\+)'\
+	| awk -F':' 'BEGIN {}\
+	{ prevPathArrLen=split(prevPath, prevPathArr, "/"); pathArrLen=split($3, pathArr, "/"); }
+	{ for(i = 1; i <= prevPathArrLen && i <= pathArrLen; i++) { if(prevPathArr[i] != pathArr[i]) {break;} }
+		for(depth = i; depth < pathArrLen; depth++) {
+			dirPath = pathArr[i];
+			for(j = i+1; j <= depth; j++) { dirPath = dirPath "/" pathArr[j] }
+			print " :d:" dirPath;
+		}
+	}
+	{ prevPath=$3; print; }' \
+	 | awk -F':' '
+		{ pathLen=split($3, path, "/"); out=$1 " " }
+		{ for(i = 1; i < pathLen; i++) { if(i == pathLen - 1) { out = out "└──" } else { out = out "│  " } } }
+		 $2 == "d" {out = out "🗀 " }
+		# $2 == "d" {out = out "📁 " }
+		 $2 == "f" {out = out " " }
+		#$2 == "f" {out = out "🗎 " }
+		{ out = out path[pathLen] }
+		{ for( i = 1; i < length(lastOut) && i < length(out); i++) {
+			lastChar = substr(lastOut, i, 1);
+			curChar = substr(out, i ,1);
+			if((lastChar == "└") &&
+				(curChar == "│" || curChar == "└")) {
+				lastOut = substr(lastOut, 0, i-1) "├" substr(lastOut, i+1);
+			}
+		}}
+		{ print lastOut; lastOut=out }
+		END { print lastOut }
+		'
--- arch/server/usr/bin/post-samba-updates.sh
+++ arch/server/usr/bin/post-samba-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SLACK_WEBHOOK_URL=""
-CHANGED_FILES="$(diff-dirs /srv/backup/samba/$(date -d '-1 day' +%Y-%m-%d) /srv/samba)"
+CHANGED_FILES="$(diff-dirs /srv/backup/samba/$(date -d '-1 day' +%Y-%m-%d)/fresh4less /srv/samba/fresh4less)"
 if [ -n "$CHANGED_FILES" ]; then
 	DATA='{"text":">>>'$CHANGED_FILES'"}'
 	echo $DATA
