8bc5582426058ca6fa368511adf06f5b12390281
Elliot Hatch <elliot.hatch@gmail.com>
2020-08-15 03:57:07 -0600

    Add data subvolume for game server installs, etc

--- arch/server/setup.sh
+++ arch/server/setup.sh
@@ -19,9 +19,21 @@ sudo cp $DIR/etc/systemd/system/ddns.timer /etc/systemd/system
 sudo systemctl start ddns.timer
 sudo systemctl enable ddns.timer
 
-# create backup volume
+# TODO: add commands for setting up RAID
+# set up subvolumes
 sudo mount -t btrfs /dev/sda1 /mnt/btrfs
+# create samba volume
+sudo btrfs subvolume create /mnt/btrfs/samba
+sudo mkdir /srv/samba
+sudo mount -t btrfs -o subvol=samba /dev/sda1 /srv/samba
+# create backup volume
 sudo btrfs subvolume create /mnt/btrfs/backup
+sudo mkdir /srv/backup
+sudo mount -t btrfs -o subvol=backup /dev/sda1 /srv/backup
+# create data volume (not backed up, e.g. game server files)
+sudo btrfs subvolume create /mnt/btrfs/data
+sudo mkdir /srv/data
+sudo mount -t btrfs -o subvol=data /dev/sda1 /srv/data
 sudo umount /mnt/btrfs
 
 sudo cp $DIR/etc/systemd/system/backup-btrfs.service /etc/systemd/system
