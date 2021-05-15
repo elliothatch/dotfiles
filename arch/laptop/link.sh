0b16697a52393962096bf304b4224a406d54f6b8
Elliot Hatch <elliot.hatch@gmail.com>
2021-05-14 18:05:37 -0600

    Restore laptop screensaver settings, increase alacritty font size

--- arch/laptop/link.sh
+++ arch/laptop/link.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-ln -svirn $DIR/.config/* $HOME/.config
+ln -svirn $DIR/.config/* $HOME/.config/
 ln -svirn $DIR/.local/bin/* $HOME/.local/bin
 ln -svirn $DIR/dotfiles/.* $HOME
