a4edf06b15d1abe2ad27df6c48d7540e27288ea9
Elliot Hatch <elliot.hatch@gmail.com>
2024-05-24 20:08:13 -0600

    Replace system beep with sound effect

--- core/link.sh
+++ core/link.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 mkdir -p $HOME/.config
 mkdir -p $HOME/.local/bin
 
-ln -svirn $DIR/.config/* $HOME/.config
+ln -svirn $DIR/.config/*[^pulse] $HOME/.config
+ln -svirn $DIR/.config/pulse/default.pa $HOME/.config/pulse/default.pa
 ln -svirn $DIR/.local/bin/* $HOME/.local/bin
 ln -svirn $DIR/dotfiles/.* $HOME
