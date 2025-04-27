9aaac38f552064be6b4f20114250a75572c3e67a
Elliot Hatch <elliot.hatch@gmail.com>
2025-04-26 20:10:32 -0600

    Fix labyrinth setup paths

--- labyrinth/setup.sh
+++ labyrinth/setup.sh
@@ -3,16 +3,16 @@ trap "exit" INT
 # setup.sh is meant to be run once you have completed Arch Linux installation and are booted into the actual OS on disk for the first time as the root user
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-$DIR/core/setup.sh
+$DIR/../core/setup.sh
+
 # amd
 sudo pacman -S amd-ucode
 # radeon
 sudo pacman -S mesa vulkan-radeon xf86-video-amdgpu
 
-
-$DIR/wayland/setup.sh
-$DIR/icebox-client/setup.sh
-$DIR/applications/setup.sh
+$DIR/../wayland/setup.sh
+$DIR/../icebox-client/setup.sh
+$DIR/../applications/setup.sh
 
 # mpd
 mkdir -p $HOME/.local/share/mpd
